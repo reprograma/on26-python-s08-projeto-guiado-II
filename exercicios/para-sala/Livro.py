4831f0540239abc4ad89ee48719fa76f2c64b077
Ferreira Mayara <fmayara@lenovo.com>
2023-09-25 23:53:44 -0300

    adicionando docstrings nas classes Livro e Biblioteca, adicionando setUp, adicionando código de exemplo de uso das classes no arquivo main.py

--- exercicios/para-sala/Livro.py
+++ exercicios/para-sala/Livro.py
@@ -1,5 +1,14 @@
 class Livro:
-    def __init__(self, nome, autor):
+    """Classe biblioteca.
+    
+    Recebe no construtor os parametros nome e autor como o exemplo:
+    
+    livro = Livro("nome livro","autor livro")
+    """
+    def __init__(self, nome, autor):        
         self.nome = nome
+        """propriedade nome do livro"""
         self.autor = autor
+        """propriedade autor do livro"""
         self.esta_emprestado = False
+        """propriedade esta_emprestado. Valor padrão é falso"""
--- exercicios/para-sala/testBiblioteca.py
+++ exercicios/para-sala/testBiblioteca.py
@@ -3,31 +3,30 @@ from Biblioteca import Biblioteca
 from Livro import Livro
 
 class TestBiblioteca(TestCase):
-    def test_init_deve_passar(self):
-        # Arrange / Act
-        biblioteca = Biblioteca()
+    def setUp(self): # Esse método é chamado antes de cada um dos testes dessa classe
+        self.biblioteca = Biblioteca()
 
+    def test_init_deve_passar(self):
+        # Arrange / Act feitos no setup()
         # Assert
-        self.assertIsInstance(biblioteca.livros, list)
+        self.assertIsInstance(self.biblioteca.livros, list)
     
     def test_adicionar_livro_deve_passar(self):
         # Arrange 
-        biblioteca = Biblioteca()
         nome_livro = "O mito da beleza"
         autor_livro = "Naomi Wolf"
         livro = Livro(nome_livro, autor_livro)
 
         # Act
-        biblioteca.adicionar_livro(livro)
+        self.biblioteca.adicionar_livro(livro)
 
         # Assert
-        self.assertEqual(1, len(biblioteca.livros))
+        self.assertEqual(1, len(self.biblioteca.livros))
 
     def test_adicionar_livro_nao_deve_inserir_numero(self):
         # Arrange
-        biblioteca = Biblioteca()
         livro = 1988
 
         # Act / Assert
         with self.assertRaises(TypeError):
-            biblioteca.adicionar_livro(livro)
+            self.biblioteca.adicionar_livro(livro)
