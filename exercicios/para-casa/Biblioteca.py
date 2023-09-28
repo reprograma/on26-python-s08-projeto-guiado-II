62dc51e561e9c55a5e69801a68c78e6f351a3573
larissalemos <larissa.lemos.souza@gmail.com>
2023-09-28 19:55:05 -0300

    Correções pós entrega - Larissa

--- exercicios/para-casa/Biblioteca.py
+++ exercicios/para-casa/Biblioteca.py
@@ -30,30 +30,44 @@ class Biblioteca:
         if (not isinstance(livro, Livro)):
             raise TypeError(f"Esperado Livro, obtido valor {livro} do tipo {type(livro)}")
         self.livros.append(livro)
+        return f"O {livro_objeto.nome}, do(a) autor(a) {livro_objeto.autor} foi adicionado ao catálogo da biblioteca."
 
     #Criar método exibir_livros
     def exibir_livro(self):
-        return self.livros
+        return [livro.nome for livro in self.livros]
         
     #Criar método emprestar_livro
     def emprestar_livro(self, livro: Livro):
-        if livro.nome not in self.livros:
-            print(f"Livro não existente no catálogo.")
-        else:
-            if livro.esta_emprestado == True:
-                print(f"{livro.nome} já está emprestado.")
+        if livro in self.livros:
+            if livro.esta_emprestado:
+                return f"{livro.nome} já está emprestado."
             else:
                 livro.esta_emprestado = True
-
+                return f"{livro.nome} foi emprestado a você."
+        else:
+            return f"Livro não existente no catálogo."
 
 nome_livro = "O mito da beleza"
 autor_livro = "Naomi Wolf"
-livro_objeto = Livro(nome = nome_livro, autor = autor_livro)
 
+# Instanciando o objeto livro_objeto da classe Livro, passando as variáveis nome_livro e autor_livro
+livro_objeto = Livro(nome = nome_livro, autor = autor_livro)
+print(f"Criamos o objeto livro_objeto da classe Livro com os valores nome = {livro_objeto.nome} e autor = {livro_objeto.autor}\n")
+
+# Instanciando o objeto biblioteca_objeto da classe Biblioteca 
 biblioteca_objeto = Biblioteca()
 
-print(biblioteca_objeto.livros)
-
+# Cadastrando o livro criado anteriormente no objeto livro_objeto
+# passamos o objeto livro_objeto para o método adicionar_livro do objeto biblioteca_objeto
+# que por sua vez adiciona o objeto livro_objeto na lista de livros
+biblioteca_objeto.adicionar_livro(livro_objeto)
+      
+# Exibe o livro adicionado ao acervo
 for livro in biblioteca_objeto.livros:
-    print(livro.nome)
-    print(livro.autor)
+    print(f"O livro {livro.nome} do(a) autor(a) {livro.autor} foi adicionado ao acervo")
+
+print(f"A lista de livros contidos na biblioteca é:\n{biblioteca_objeto.exibir_livro()}")
+
+print(biblioteca_objeto.emprestar_livro(livro_objeto))
+
+biblioteca_objeto.emprestar_livro(livro_objeto)
